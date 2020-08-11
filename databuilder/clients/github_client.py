613ea58785f58f8165495d665257d2cdc8c6943a
Andrew <andrjc4@vt.edu>
2020-08-11 14:03:25 -0400

    add more debugging statements

--- databuilder/clients/github_client.py
+++ databuilder/clients/github_client.py
@@ -36,6 +36,7 @@ class GithubClient:
             headers=self._request_headers,
             auth=self._request_auth
         )
+        print(response.status_code)
         response_json = response.json()
         for file_object in response_json:
             if _is_file_object_a_directory(file_object):
